93505c2fd45746a4267d5b1c0ef5238b0002d4d7
fee-sah-yor <101174144+fee-sah-yor@users.noreply.github.com>
2024-04-08 18:22:52 +0100

    added validation to the form

--- src/App.js
+++ src/App.js
@@ -1,128 +1,19 @@
-import "./App.css";
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from './component/SignUp';
+import Login from './component/Login';
 
-export const PasswordValidation = () => {
-  return <p className='passVal'>* Password must be up to or more than 8 characters</p>;
-};
 
-function App() {
-  const [details, setDetails] = useState({
-    email: "",
-    password: "",
-    confirm: "",
-  });
-  const [error, setError] = useState(false);
-  const [detailsArr, setDetailsArr] = useState([]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDetails((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-    // to check for errors while inputing email
-    if (email.includes("@", ".com")) {
-      setError(false);
-    } else {
-      setError(true);
-    }
-  };
-
-  let { email, password, confirm } = details;
-  const handleSubmit = () => {
-    setDetailsArr([...detailsArr, { email, confirm, password }]);
-    // to clear form after submission
-    setDetails({ email: "", confirm: "", password: "" });
-    // to check if both passwords match
-    if (confirm !== password && email === ' ') {
-      setError(true);
-    } else {
-      setError(false);
-    }
-  };
-
-  const formValid = () => {
-      if(email === ' '){
-        return false
-      } 
-      return true;
-  }
+const App = () => {
 
   return (
-    <div>
-      <section>
-        <form>
-          {/* Form header */}
-
-          <div className="formHeader">
-            <h1>Sign Up</h1>
-            <p>Log in with the data provided during your registeration</p>
-          </div>
-          {/* =========error message=========== */}
-          {error && (
-            <div className="error">
-              <p>Wrong password or email address</p>
-            </div>
-          )}
-
-          {/* input field */}
-          <article className="inputField">
-            <div className="email">
-              <p>Email</p>
-              <input
-                type="email"
-                name="email"
-                required
-                placeholder="your email address"
-                onChange={handleChange}
-                value={details.email}
-              />
-            </div>
-            <div className="password">
-              <p>Password</p>
-              <input
-                type="Password"
-                name="password"
-                required
-                placeholder="type your password"
-                onChange={handleChange}
-                value={details.password}
-              />
-              {details.password.length < 8 ? <PasswordValidation /> : null}
-            </div>
-            <div className="c_Password">
-              <p>Confirm password</p>
-              <input
-                type="password"
-                name="confirm"
-                required
-                placeholder="re-type your password"
-                onChange={handleChange}
-                value={details.confirm}
-              />
-            </div>
-          </article>
-
-          <article className="helpLinks">
-            <div className="remember">
-              <input className="checkBox" type="checkbox" />
-              <p>Remember me?</p>
-            </div>
-            <div className="forgot">
-              <p>Forgot password</p>
-            </div>
-          </article>
-
-          <article className="btnCont">
-            <button className="signIn" onClick={handleSubmit} disabled={!formValid()}>
-              Sign Up
-            </button>
-            <button className="logIn">Log In</button>
-          </article>
-        </form>
-      </section>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter> 
   );
-}
+};
 
 export default App;
---
+++ src/component/SignUp.js
@@ -0,0 +1,115 @@
+import React from "react";
+import "../App.css";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+export const FormErrorMessage = ({ children }) => {
+  return <p className="passVal">*{children}</p>;
+};
+
+const SignUp = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirm: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "password must be above 8 characters")
+        .required("Required"),
+    confirm: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Required')
+    }),
+  });
+
+  if(!formik.errors){
+    console.log('succesful')
+  } 
+
+  return (
+    <div>
+      <section>
+        <form onSubmit={(e) => {e.preventDefault();
+            formik.handleSubmit()}}>
+          {/* Form header */}
+
+          <div className="formHeader">
+            <h1>Sign Up</h1>
+            <p>Log in with the data provided during your registeration</p>
+          </div>
+
+          {/* input field */}
+          <article className="inputField">
+            <div className="email">
+              <p>Email</p>
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="your email address"
+                {...formik.getFieldProps("email")}
+              />
+              {formik.touched.email && formik.errors.email ? (
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              ) : null}
+            </div>
+            <div className="password">
+              <p>Password</p>
+              <input
+                type="Password"
+                name="password"
+                required
+                placeholder="type your password"
+                {...formik.getFieldProps("password")}
+              />
+              {formik.touched.password && formik.errors.password ? (
+                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+              ) : null}
+            </div>
+
+            <div className="c_Password">
+              <p>Confirm password</p>
+              <input
+                type="password"
+                name="confirm"
+                required
+                placeholder="re-enter your password"
+                {...formik.getFieldProps("confirm")}
+              />
+              {formik.touched.confirm && formik.errors.confirm ? (
+                <FormErrorMessage>{formik.errors.confirm}</FormErrorMessage>
+              ) : null}
+            </div>
+          </article>
+
+          <article className="helpLinks">
+            <div className="remember">
+              <input className="checkBox" type="checkbox" />
+              <p>Remember me?</p>
+            </div>
+            <div className="forgot">
+              <p>Forgot password?</p>
+            </div>
+          </article>
+
+          <article className="btnCont">
+            <button
+              className="signIn"
+              type="submit"
+            >
+              Sign Up
+            </button>
+            <button className="logIn">Log In</button>
+          </article>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default SignUp;
